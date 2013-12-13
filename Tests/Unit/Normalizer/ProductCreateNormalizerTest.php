34d1caf6d1d9a0ad4380db48757c59c6992e6f98
sumbobyboys <julien@akeneo.com>
2013-12-13 17:38:27 +0100

    Fixtures and auto-matching for simple magento install

--- Tests/Unit/Normalizer/ProductCreateNormalizerTest.php
+++ Tests/Unit/Normalizer/ProductCreateNormalizerTest.php
@@ -52,6 +52,7 @@ class ProductCreateNormalizerTest extends \PHPUnit_Framework_TestCase
                     'red' => 3
                 )
             ),
+            'taxClassId'               => 4,
             'defaultLocale'            => 'en_US',
             'channel'                  => 'channel',
             'website'                  => 'base',
