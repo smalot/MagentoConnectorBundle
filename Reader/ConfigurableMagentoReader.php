85e4350071a4d1409b175c9d17e0e709399b591d
sumbobyboys <julien@akeneo.com>
2014-01-08 10:38:00 +0100

    [WIP] New group repository

--- Reader/ConfigurableMagentoReader.php
+++ Reader/ConfigurableMagentoReader.php
@@ -14,7 +14,7 @@ use Pim\Bundle\CatalogBundle\Entity\Channel;
 /**
  * Product reader
   *
-  * @author    Gildas Quemener <gildas@akeneo.com>
+  * @author    Julien Sanchhez <julien@akeneo.com>
   * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
   * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
   */
---
+++ Repository/GroupRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pim\Bundle\MagentoConnectorBundle\Repository;
+
+use Pim\Bundle\CatalogBundle\Entity\Repository\GroupRepository as BaseGroupRepository;
+
+/**
+ * Group repository
+ *
+ * @author    Julien Sanchez <julien@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class GroupRepository extends BaseGroupRepository
+{
+
+}
