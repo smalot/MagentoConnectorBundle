2f2a28f22361c3592f213c7bd67a9c559c01662c
Nicolas Dupont <nicolas@akeneo.com>
2014-02-04 15:47:15 +0100

    PIM-1883 : execute valid currency and valid locale validators only in the execution group (in default group, we cant create the job instance)

--- Processor/AbstractProductProcessor.php
+++ Processor/AbstractProductProcessor.php
@@ -18,8 +18,8 @@ use Pim\Bundle\MagentoConnectorBundle\Manager\CurrencyManager;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
- * @HasValidDefaultLocale()
- * @HasValidCurrency()
+ * @HasValidDefaultLocale(groups={"Execution"})
+ * @HasValidCurrency(groups={"Execution"})
  */
 abstract class AbstractProductProcessor extends AbstractProcessor
 {
