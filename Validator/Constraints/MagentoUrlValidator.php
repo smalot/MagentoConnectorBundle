7a7e285060d27e8d39abb75c91d91bda6d2073ed
Olivier Soulet <olivier.soulet@akeneo.com>
2014-04-09 12:46:49 +0200

    Fix soap url #98
    Soap url must end by a trailing slash.
    MagentoUrlValidato is now able to check that.

--- Validator/Constraints/MagentoUrlValidator.php
+++ Validator/Constraints/MagentoUrlValidator.php
@@ -35,6 +35,10 @@ class MagentoUrlValidator extends ConstraintValidator
      */
     public function isValidMagentoUrl($url)
     {
+        if (substr($url, strlen($url)-1,1) != '/') {
+            return false;
+        }
+
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url . MagentoSoapClient::SOAP_WSDL_URL);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
