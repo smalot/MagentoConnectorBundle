6dbaaead0ff4ed031d88e946294f138950bf0c2d
Olivier Soulet <olivier.soulet@akeneo.com>
2014-04-22 16:52:39 +0200

    Added use for autocomplete #104

--- Item/MagentoItemStep.php
+++ Item/MagentoItemStep.php
@@ -2,6 +2,7 @@
 
 namespace Pim\Bundle\MagentoConnectorBundle\Item;
 
+use Pim\Bundle\MagentoConnectorBundle\Webservice\Webservice;
 use Symfony\Component\Validator\Constraints as Assert;
 use Akeneo\Bundle\BatchBundle\Item\AbstractConfigurableStepElement;
 use Pim\Bundle\MagentoConnectorBundle\Guesser\WebserviceGuesser;
