476826a2eed0dc022bc5cf0ba555144aa3ed253f
willy-ahva <willy.mesnage@akeneo.com>
2014-04-23 15:41:16 +0200

    Add a space in AttributeNormalizer #105

--- Normalizer/AttributeNormalizer.php
+++ Normalizer/AttributeNormalizer.php
@@ -184,7 +184,7 @@ class AttributeNormalizer implements NormalizerInterface
             throw new InvalidAttributeNameException(
                 sprintf(
                     'The attribute "%s" have a code that is not compatible with Magento. Please use only' .
-                    'lowercase letters (a-z), numbers (0-9) or underscore(_). First caracter should also' .
+                    ' lowercase letters (a-z), numbers (0-9) or underscore(_). First caracter should also' .
                     ' be a letter and your attribute codelength must be under 30 characters',
                     $attribute->getCode()
                 )
