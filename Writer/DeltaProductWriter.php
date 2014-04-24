52cb040c4eef82bd39115d6e37761a9671242e44
sumbobyboys <julien@akeneo.com>
2014-04-24 18:39:53 +0200

    little fixes

--- Writer/DeltaProductWriter.php
+++ Writer/DeltaProductWriter.php
@@ -12,10 +12,10 @@ use Pim\Bundle\DeltaExportBundle\Manager\ProductExportManager;
 use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
 
 /**
- * Magento product writer
+ * Magento delta product writer
  *
  * @author    Julien Sanchez <julien@akeneo.com>
- * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class DeltaProductWriter extends ProductWriter
