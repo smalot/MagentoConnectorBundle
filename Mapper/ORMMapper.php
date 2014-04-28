bdf4754bcaefc85aeaf4240397e15e8aa953219f
willy-ahva <willy.mesnage@akeneo.com>
2014-04-28 18:35:44 +0200

    Replace Mapper by MagentoMapper #108
    MagentoMapper extends ConnectorMapping\Mapper\Mapper
    Classes which extended Mapper extend now MagentoMapper

--- Mapper/ORMMapper.php
+++ Mapper/ORMMapper.php
@@ -13,7 +13,7 @@ use Pim\Bundle\ConnectorMappingBundle\Mapper\MappingCollection;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ORMMapper extends Mapper
+class ORMMapper extends MagentoMapper
 {
     /**
      * @var SimpleMappingManager
