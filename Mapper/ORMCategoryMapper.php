6bfce3198097f855ae8aacc3c3b029345e05b668
willy-ahva <willy.mesnage@akeneo.com>
2014-04-30 11:51:04 +0200

    Replace ORMMapper bu ORMPimMapper #108
    Remove MCB\Mapper\ORMMapper
    ADD MCB\Mapper\ORMPimMapper extends CMB\Mapper\ORMMapper

--- Mapper/ORMCategoryMapper.php
+++ Mapper/ORMCategoryMapper.php
@@ -14,7 +14,7 @@ use Pim\Bundle\CatalogBundle\Model\CategoryInterface;
  * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ORMCategoryMapper extends ORMMapper
+class ORMCategoryMapper extends ORMPimMapper
 {
     /**
      * @var CategoryManager
