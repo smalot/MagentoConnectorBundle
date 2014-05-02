f4a621d3fab6a056a5f73b8d7b0907c3cd501ad0
Olivier Soulet <olivier.soulet@akeneo.com>
2014-05-02 16:07:01 +0200

    Add tests for groups export and delete #104
    *test for group export
    *test for group deletion on magento side
    *mapping test
    *exluded groups checked

---
+++ spec/Pim/Bundle/MagentoConnectorBundle/Cleaner/FamilyCleanerSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\Pim\Bundle\MagentoConnectorBundle\Cleaner;
+
+use Pim\Bundle\MagentoConnectorBundle\Guesser\WebserviceGuesser;
+use Pim\Bundle\MagentoConnectorBundle\Manager\FamilyMappingManager;
+use Pim\Bundle\MagentoConnectorBundle\Webservice\Webservice;
+use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FamilyCleanerSpec extends ObjectBehavior
+{
+    function let(
+        WebserviceGuesser      $webserviceGuesser,
+        FamilyMappingManager   $familyMappingManager,
+        Webservice             $webservice,
+        StepExecution          $stepExecution
+    ) {
+        $this->beConstructedWith($webserviceGuesser, $familyMappingManager);
+        $this->setStepExecution($stepExecution);
+
+        $webserviceGuesser->getWebservice(Argument::cetera())->willReturn($webservice);
+    }
+
+    function it_asks_soap_client_to_delete_families_that_are_not_in_pim_anymore($webservice, $familyMappingManager)
+    {
+        $this->setNotInPimAnymoreAction('delete');
+
+        $webservice->getAttributeSetList()->willReturn(
+            array(
+                'family set name 1' => 1,
+                'family set name 5' => 5,
+                'family set name 151' => 151
+            )
+        );
+
+        $familyMappingManager->magentoFamilyExists(1, Argument::cetera())->shouldBeCalled()->willReturn(true);
+        $familyMappingManager->magentoFamilyExists(5, Argument::cetera())->shouldBeCalled()->willReturn(true);
+        $familyMappingManager->magentoFamilyExists(151, Argument::cetera())->shouldBeCalled()->willReturn(false);
+
+        $webservice->removeAttributeSet('151')->shouldBeCalled();
+
+        $this->execute();
+    }
+}
