c962b7067d18d977ce6f02fcf9a9c0337ebc4ac6
willy-ahva <willy.mesnage@akeneo.com>
2014-05-07 12:47:53 +0200

    Add spec for HasValidCurrencyValidator #121

--- Validator/Constraints/HasValidCurrencyValidator.php
+++ Validator/Constraints/HasValidCurrencyValidator.php
@@ -5,6 +5,7 @@ namespace Pim\Bundle\MagentoConnectorBundle\Validator\Constraints;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Pim\Bundle\CatalogBundle\Manager\ChannelManager;
+use Pim\Bundle\MagentoConnectorBundle\Processor\AbstractProductProcessor;
 
 /**
  * Validator for currency
@@ -31,17 +32,21 @@ class HasValidCurrencyValidator extends ConstraintValidator
     /**
      * Checks if the passed value is valid.
      *
-     * @param AbstractConfigurableStepElement $protocol   The value that should be validated
-     * @param Constraint                      $constraint The constraint for the validation
+     * @param mixed $value   The value that should be validated
+     * @param Constraint     $constraint The constraint for the validation
      *
      * @api
      * @return mixed
      */
-    public function validate($protocol, Constraint $constraint)
+    public function validate($value, Constraint $constraint)
     {
-        if ($channel = $this->channelManager->getChannelByCode($protocol->getChannel())) {
+        if (!$value instanceof AbstractProductProcessor) {
+            return null;
+        }
+
+        if ($channel = $this->channelManager->getChannelByCode($value->getChannel())) {
             foreach ($channel->getCurrencies() as $currency) {
-                if ($currency->getCode() === $protocol->getCurrency()) {
+                if ($currency->getCode() === $value->getCurrency()) {
                     return true;
                 }
             }
---
+++ spec/Pim/Bundle/MagentoConnectorBundle/Validator/Constraints/HasValidCurrencyValidatorSpec.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace spec\Pim\Bundle\MagentoConnectorBundle\Validator\Constraints;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ExecutionContextInterface;
+use Pim\Bundle\MagentoConnectorBundle\Processor\AbstractProductProcessor;
+use Pim\Bundle\CatalogBundle\Manager\ChannelManager;
+use Pim\Bundle\MagentoConnectorBundle\Validator\Constraints\HasValidCurrency;
+use Pim\Bundle\CatalogBundle\Entity\Channel;
+use Pim\Bundle\CatalogBundle\Entity\Currency;
+
+
+/**
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class HasValidCurrencyValidatorSpec extends ObjectBehavior
+{
+    function let(ExecutionContextInterface $context, ChannelManager $channelManager)
+    {
+        $this->beConstructedWith($channelManager);
+        $this->initialize($context);
+    }
+
+    function it_does_nothing_with_something_else_than_abstract_product_element(
+        $context,
+        HasValidCurrency $constraint,
+        ChannelManager $value
+    ) {
+        $context->addViolationAt(Argument::cetera())->shouldNoTBeCalled();
+
+        $this->validate($value, $constraint);
+    }
+
+    function it_success_if_the_currency_is_valid(
+        $context,
+        $channelManager,
+        AbstractProductProcessor $value,
+        HasValidCurrency $constraint,
+        Channel $channel,
+        Currency $currency
+    ) {
+        $value->getChannel()->willReturn('channel');
+        $channelManager->getChannelByCode(Argument::any())->willReturn($channel);
+        $channel->getCurrencies()->willReturn(array($currency));
+        $currency->getCode()->willReturn('euro');
+        $value->getCurrency()->willReturn('euro');
+
+        $context->addViolationAt(Argument::cetera())->shouldNotBeCalled();
+
+        $this->validate($value, $constraint);
+    }
+
+    function it_fails_if_the_currency_is_not_valid(
+        $context,
+        $channelManager,
+        AbstractProductProcessor $value,
+        HasValidCurrency $constraint,
+        Channel $channel,
+        Currency $currency
+    ) {
+        $value->getChannel()->willReturn('channel');
+        $channelManager->getChannelByCode('channel')->willReturn($channel);
+        $channel->getCurrencies()->willReturn(array($currency));
+        $currency->getCode()->willReturn('dollar');
+        $value->getCurrency()->willReturn('euro');
+
+        $constraint->message = 'The given currency is not valid (check that the selected currency is in channel\'s currencies';
+
+        $context->addViolationAt('currency', 'The given currency is not valid (check that the selected currency is in channel\'s currencies', array('currency'))->shouldBeCalled();
+
+        $this->validate($value, $constraint);
+    }
+
+    function it_fails_if_no_channel_has_been_found(
+        $context,
+        $channelManager,
+        AbstractProductProcessor $value,
+        HasValidCurrency $constraint,
+        Channel $channel
+    ) {
+        $value->getChannel()->willReturn('channel');
+        $channelManager->getChannelByCode('channel')->willReturn(false);
+
+        $constraint->message = 'The given currency is not valid (check that the selected currency is in channel\'s currencies';
+
+        $context->addViolationAt('currency', 'The given currency is not valid (check that the selected currency is in channel\'s currencies', array('currency'))->shouldBeCalled();
+
+        $this->validate($value, $constraint);
+    }
+}
