35f80dbd93326aac9581d19c72bdb853c8c36c0d
willy-ahva <willy.mesnage@akeneo.com>
2014-05-14 15:09:09 +0200

    Add AttributeGroupMappingManagerSpec #145

---
+++ spec/Pim/Bundle/MagentoConnectorBundle/Manager/AttributeGroupMappingManagerSpec.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace spec\Pim\Bundle\MagentoConnectorBundle\Manager;
+
+use Pim\Bundle\CatalogBundle\Entity\AttributeGroup;
+use Pim\Bundle\CatalogBundle\Entity\Repository\GroupRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityRepository;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AttributeGroupMappingManagerSpec extends ObjectBehavior
+{
+    protected $className;
+
+    function let(ObjectManager $objectManager, $className)
+    {
+        $this->beConstructedWith($objectManager, $className);
+        $this->className = $className;
+    }
+
+    function it_gives_group_from_id(EntityRepository $repository, $objectManager)
+    {
+        $objectManager->getRepository($this->className)->willReturn($repository);
+        $repository->findOneById(array(
+                'magentoGroupId' => 2,
+                'magentoUrl'     => 'http://magento.url'
+            )
+        )->willReturn();
+
+    }
+}
