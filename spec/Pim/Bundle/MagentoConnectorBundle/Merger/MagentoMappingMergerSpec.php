a6fc1e24344e3fa7e193d60248609140b970e326
willy-ahva <willy.mesnage@akeneo.com>
2014-05-27 16:32:46 +0200

    Refactor MagentoMappingMergerSpec to use registry #166

--- spec/Pim/Bundle/MagentoConnectorBundle/Merger/MagentoMappingMergerSpec.php
+++ spec/Pim/Bundle/MagentoConnectorBundle/Merger/MagentoMappingMergerSpec.php
@@ -2,7 +2,7 @@
 
 namespace spec\Pim\Bundle\MagentoConnectorBundle\Merger;
 
-use Pim\Bundle\MagentoConnectorBundle\Webservice\MagentoSoapClientParameters;
+use Pim\Bundle\MagentoConnectorBundle\Webservice\MagentoSoapClientParametersRegistry;
 use Pim\Bundle\MagentoConnectorBundle\Mapper\MagentoMapper;
 use PhpSpec\ObjectBehavior;
 
@@ -14,7 +14,7 @@ class MagentoMappingMergerSpec extends ObjectBehavior
         $mapper2->getPriority()->willReturn(10);
     }
 
-    function it_sets_parameters_to_all_mappers($mapper1, $mapper2, MagentoSoapClientParameters $clientParameters)
+    function it_sets_parameters_to_all_mappers($mapper1, $mapper2, MagentoSoapClientParametersRegistry $clientParameters)
     {
         $this->beConstructedWith(array($mapper1, $mapper2), 'generic', 'export', true);
 
@@ -24,7 +24,7 @@ class MagentoMappingMergerSpec extends ObjectBehavior
         $this->setParameters($clientParameters, 'default');
     }
 
-    function it_gives_a_configuration_field($mapper1, $mapper2, MagentoSoapClientParameters $clientParameters)
+    function it_gives_a_configuration_field($mapper1, $mapper2, MagentoSoapClientParametersRegistry $clientParameters)
     {
         $this->beConstructedWith(array($mapper2, $mapper1), 'generic', 'export', true);
 
