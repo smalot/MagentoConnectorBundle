f58d160c3879377dab2e5c09a34ae69f76dc8b58
willy-ahva <willy.mesnage@akeneo.com>
2014-11-05 16:02:37 +0100

    Refactor getValidProducts() in ValidProductHelper

--- Helper/ValidProductHelper.php
+++ Helper/ValidProductHelper.php
@@ -29,28 +29,60 @@ class ValidProductHelper
         $rootCategoryId = $channel->getCategory()->getId();
 
         foreach ($products as $product) {
-            $isComplete = true;
-            $completenesses = $product->getCompletenesses()->getIterator();
-            while ((list($key, $completeness) = each($completenesses)) && $isComplete) {
-                if ($completeness->getChannel()->getId() === $channel->getId() &&
-                    $completeness->getRatio() < 100
-                ) {
-                    $isComplete = false;
-                }
-            }
+            $isProductComplete = $this->isProductComplete($product, $channel);
 
             $productCategories = $product->getCategories()->getIterator();
-            if ($isComplete && false !== $productCategories) {
-                $isInChannel = false;
-                while ((list($key, $category) = each($productCategories)) && !$isInChannel) {
-                    if ($category->getRoot() === $rootCategoryId) {
-                        $isInChannel = true;
-                        $validProducts[] = $product;
-                    }
-                }
+            if ($isProductComplete &&
+                false !== $productCategories &&
+                $this->isProductBelongToChannel($productCategories, $rootCategoryId)
+            ) {
+                $validProducts[] = $product;
             }
         }
 
         return $validProducts;
+    }
+
+    /**
+     * Is the given product is complete for the given channel ?
+     *
+     * @param ProductInterface $product
+     * @param Channel          $channel
+     *
+     * @return bool
+     */
+    protected function isProductComplete(ProductInterface $product, Channel $channel)
+    {
+        $isComplete = true;
+        $completenesses = $product->getCompletenesses()->getIterator();
+        while ((list($key, $completeness) = each($completenesses)) && $isComplete) {
+            if ($completeness->getChannel()->getId() === $channel->getId() &&
+                $completeness->getRatio() < 100
+            ) {
+                $isComplete = false;
+            }
+        }
+
+        return $isComplete;
+    }
+
+    /**
+     * Compute the belonging of a product to a channel with its categories
+     *
+     * @param \ArrayIterator|array $productCategories
+     * @param int                  $rootCategoryId
+     *
+     * @return bool
+     */
+    protected function isProductBelongToChannel($productCategories, $rootCategoryId)
+    {
+        $isInChannel = false;
+        while ((list($key, $category) = each($productCategories)) && !$isInChannel) {
+            if ($category->getRoot() === $rootCategoryId) {
+                $isInChannel = true;
+            }
+        }
+
+        return $isInChannel;
     }
 }
