59f46450bbb1171c64e7c369f9cfd84a4e39fa83
willy-ahva <willy.mesnage@akeneo.com>
2014-12-12 18:09:30 +0100

    MC-100: Add a reader to filter identifier attributes

---
+++ Reader/AttributeReader.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Pim\Bundle\MagentoConnectorBundle\Reader;
+
+use Pim\Bundle\BaseConnectorBundle\Reader\ORM\EntityReader;
+
+/**
+ * This attribute reader is used to retrieve attributes without identifier
+ *
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
+ * @copyright 2014 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AttributeReader extends EntityReader
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getQuery()
+    {
+        if (!$this->query) {
+            $this->query = $this->em
+                ->getRepository($this->className)
+                ->createQueryBuilder('c')
+                ->select('c')
+                ->where('c.attributeType != \'pim_catalog_identifier\'')
+                ->getQuery();
+        }
+
+        return $this->query;
+    }
+}
