93b8af2ab160b44d80ea6555a48c4f7a830d3ef3
willy-ahva <willy.mesnage@akeneo.com>
2014-12-12 17:05:35 +0100

    MC-100: Add specs

---
+++ spec/Pim/Bundle/MagentoConnectorBundle/Normalizer/AttributeNormalizerSpec.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace spec\Pim\Bundle\MagentoConnectorBundle\Normalizer;
+
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CatalogBundle\Entity\AttributeTranslation;
+use Pim\Bundle\CatalogBundle\Model\AbstractAttribute;
+use Pim\Bundle\MagentoConnectorBundle\Helper\AttributeMappingHelper;
+
+class AttributeNormalizerSpec extends ObjectBehavior
+{
+    function let(AttributeMappingHelper $mappingHelper)
+    {
+        $this->beConstructedWith($mappingHelper);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('\Pim\Bundle\MagentoConnectorBundle\Normalizer\AttributeNormalizer');
+    }
+
+    function it_returns_true_if_the_normalizer_can_support_given_data(AbstractAttribute $attribute)
+    {
+        $this->supportsNormalization($attribute, 'api_import')->shouldReturn(true);
+    }
+
+    function it_returns_false_if_the_normalizer_can_not_support_given_data($object)
+    {
+        $this->supportsNormalization($object, 'api_import')->shouldReturn(false);
+    }
+
+    function it_returns_false_if_the_normalizer_can_not_support_given_data_because_of_the_format(
+        AbstractAttribute $attribute
+    ) {
+        $this->supportsNormalization($attribute, 'foo_bar')->shouldReturn(false);
+    }
+
+    function it_normalizes_a_supported_attribute(
+        AbstractAttribute $attribute,
+        AttributeTranslation $translation,
+        $mappingHelper
+    ) {
+        $context = [
+            'defaultLocale' => 'en_US',
+            'visibility' => true
+        ];
+
+        $translation->getLabel()->willReturn('My attribute');
+
+        $attribute->getAttributeType()->willReturn('pim_catalog_text');
+        $attribute->getCode()->willReturn('attribute_code');
+        $attribute->getDefaultValue()->willReturn('value');
+        $attribute->getTranslation('en_US')->willReturn($translation);
+        $attribute->isRequired()->willReturn(true);
+        $attribute->isUnique()->willReturn(false);
+
+        $mappingHelper->getMagentoAttributeTypeFor('pim_catalog_text')->shouldBeCalled()->willReturn('text');
+
+        $this->normalize($attribute, 'api_import', $context)->shouldReturn([
+            'attribute_id'     => 'attribute_code',
+            'default'          => 'value',
+            'type'             => 'text',
+            'label'            => 'My attribute',
+            'global'           => 0,
+            'required'         => 1,
+            'visible_on_front' => 1,
+            'unique'           => 0
+        ]);
+    }
+
+    function it_does_not_normalize_an_identifier_or_a_file_attribute(AbstractAttribute $attribute, $mappingHelper)
+    {
+        $attribute->getAttributeType()->willReturn('pim_catalog_identifier');
+        $mappingHelper->getMagentoAttributeTypeFor('pim_catalog_identifier')->shouldBeCalled()->willReturn(null);
+
+        $this->normalize($attribute, 'api_import', [])->shouldReturn(null);
+    }
+} 
